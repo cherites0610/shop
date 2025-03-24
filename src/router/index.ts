a107e1df3841a2fe78c7b86d677b3d565269b23e
cherites <cherites0610@gmail.com>
2025-03-24 11:55:09 +0800

    fix:HomeView wrong

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../../view/HomeView.vue'
+import HomeView from '@/view/HomeView.vue'
 import App from '@/App.vue'
 
 const router = createRouter({
